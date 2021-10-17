4d15b8023c4278404c1daa4ba0f383d05c0463af
ManikandanKumarkp <manikandankumarkp@gmail.com>
2021-10-17 11:26:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-4uthcs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-4uthcs)
---
+++ index.js
@@ -0,0 +1,62 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+//let lighton=true;
+//let status;
+//status=(lighton==false)?'lighton':'lightoff';
+//console.log(status)
+
+//let lightison=false;
+//if(lightison ==true){
+//console.log("lightis on")
+//}else
+//{
+//  console.log("lightis off")
+//}
+
+//let mark=64;
+//if(mark>=80){
+//console.log("pass first class")
+//}
+//else if(mark>=60){
+//console.log("pass secound class")
+//}
+//else if(mark>=30){
+//console.log("pass ")
+//}
+//else if(mark<=30){
+//console.log("fail")
+//}
+
+
+today.getdate() = new date();
+
+switch(today.getdate()){
+  case 1 :
+  console.log('Monday')
+   break ;
+  case 2 :
+ console.log('Tuesday')
+ break ;
+ case 3 :
+ console.log('Wednessday')
+ break ;
+ case 4:
+ console.log('Tuesday')
+ break;
+ case 5:
+ console.log('Friday')
+ break;
+ case 6:
+ console.log('Saturday')
+ break;
+ case 7:
+ console.log('Sunday')
+ break;
+ default:
+console.log('not a date')
+}
